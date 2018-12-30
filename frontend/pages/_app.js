c722e987a9139e673d5e8d6ab7992e49315b84da
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-12-30 20:20:45 +0100

    Sets up the Apollo client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,31 @@
 import App, { container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 class SickFitsApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        return { pageProps };
+    }
+
     render() {
-        const { Component } = this.props;
+        const { Component, apollo, pageProps } = this.props;
 
         return (
             <container>
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={this.props.apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
             </container>
         )
     }
 }
 
-export default SickFitsApp;
+export default withData(SickFitsApp);
