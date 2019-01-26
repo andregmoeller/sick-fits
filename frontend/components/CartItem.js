5fa5c8fc6b13992e2587738f481b6579777b89cd
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-01-26 01:26:22 +0100

    Adds the functionality to remove items from the cart

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
     align-items: center;
@@ -28,6 +29,7 @@ const CartItem = ({ cartItem }) => (
                 <em>{cartItem.quantity} &times; {formatMoney(cartItem.item.price)}> each</em>
             </p>
         </div>
+        <RemoveFromCart id={cartItem.id} />
     </CartItemStyles>
 )
 
