9794f8f9a9977d1beea60e886cd82cc830c1a89e
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-03-03 16:14:05 +0100

    Adds some more tests that mock Apollo queries

---
+++ frontend/__tests__/PleaseSignIn.test.js
@@ -0,0 +1,61 @@
+import { mount } from 'enzyme';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import PleaseSignin from '../components/PleaseSignin';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { fakeUser } from '../lib/testUtils';
+
+const notSignedInMocks = [
+    {
+        request: {
+            query: CURRENT_USER_QUERY,
+        },
+        result: {
+            data: {
+                me: null,
+            }
+        },
+    },
+];
+
+const signedInMocks = [
+    {
+        request: {
+            query: CURRENT_USER_QUERY,
+        },
+        result: {
+            data: {
+                me: fakeUser(),
+            }
+        },
+    },
+];
+
+describe('<PleaseSignIn/>', () => {
+    it('renders the sign in dialog to logged out users', async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={notSignedInMocks}>
+                <PleaseSignin />
+            </MockedProvider>
+        );
+        await wait();
+        wrapper.update();
+        expect(wrapper.text()).toContain('Please Sign In before Continuing');
+        const SignIn = wrapper.find('Signin');
+        expect(SignIn.exists()).toBe(true);
+    });
+
+    it('renders the child component when the user is signed in', async () => {
+        const Hey = () => <p>Hey!</p>;
+        const wrapper = mount(
+            <MockedProvider mocks={signedInMocks}>
+                <PleaseSignin>
+                    <Hey />
+                </PleaseSignin>
+            </MockedProvider>
+        );
+        await wait();
+        wrapper.update();
+        expect(wrapper.contains(<Hey />)).toBe(true);
+    });
+});
